89877e30dc854f93ddb0ae70d9e2eb2f60c8ea0a
chroju <chor.chroju@gmail.com>
2019-07-29 21:50:54 +0900

    Update setup process

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if !(type "brew" > /dev/null 2>&1); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 brew bundle
 ansible-playbook playbook.yml -K
 
