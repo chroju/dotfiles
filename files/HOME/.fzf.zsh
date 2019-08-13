864cec7e2c9e7dfb597128c1dc176f23830f4ff1
chroju <chor.chroju@gmail.com>
2019-08-13 15:26:13 +0900

    Add config about fzf with pushd

--- files/HOME/.fzf.zsh
+++ files/HOME/.fzf.zsh
@@ -42,3 +42,9 @@ ftm() {
   session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
 }
 
+fcd() {
+  dir_no=$(dirs -v | fzf | cut -f 1)
+  if [[ $dir_no -ne "" ]]; then
+    pushd +$dir_no
+  fi
+}
