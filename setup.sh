d7f0519635e3c1c32971d89edd232602bd83b77a
chroju <chroju@users.noreply.github.com>
2020-09-22 21:26:45 +0900

    Modify homebrew tasks, move to ansible

--- setup.sh
+++ setup.sh
@@ -5,8 +5,3 @@ fi
 brew bundle
 ansible-playbook playbook.yml -K
 
-# https://qiita.com/mottox2/items/581869563ce5f427b5f6
-cat ./files/vscode/extensions | while read line
-do
-  code --install-extension $line
-done
