cf9d149c5fd8430173d6d9c4389dbd6c9c9c0f19
chroju <chroju@users.noreply.github.com>
2021-08-18 16:05:22 +0900

    feat: spotlight index disabled

--- setup_defaults.sh
+++ setup_defaults.sh
@@ -3,3 +3,6 @@
 # https://github.com/VSCodeVim/Vim#mac
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+
+# disable spotlight index
+mdutil -a -i off
