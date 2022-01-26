b94f19ac7ee7c30132a8112edd6f1665b0eccd78
chroju <chroju@users.noreply.github.com>
2022-01-26 09:58:57 +0900

    feat: add github xbar script

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -8,20 +8,20 @@ fi
 
 function make_symlinks() {
     for f in $(ls -A $1); do
-	      src=$1/$f
-        dst=$2/$f
+	      src="$1/$f"
+        dst="$2/$f"
         if [ -f $src ]; then
-            if [ -e $dst ] && [ "$3" = '-f' ]; then
+            if [ -e "$dst" ] && [ "$3" = '-f' ]; then
                 echo "### WARN: overwrite $dst ###"
-                ln -fs $PWD/$src $dst
-            elif [ ! -e $dst ]; then
+                ln -fs $PWD/$src "$dst"
+            elif [ ! -e "$dst" ]; then
                 echo "make symlink $dst => $src ..."
-                ln -fs $PWD/$src $dst
+                ln -fs $PWD/$src "$dst"
             fi
         elif [ -d $src ]; then
-            if [ ! -e $dst ]; then
-                echo "make directory $dst ..."
-                mkdir $dst
+            if [ ! -e "$dst" ]; then
+                echo "make directory "$dst" ..."
+                mkdir "$dst"
             fi
             make_symlinks $src $2/$f $3
         fi
@@ -33,5 +33,6 @@ if [ "$1" = '-f' ]; then
 fi
 
 make_symlinks dotfiles/HOME $HOME $1
-make_symlinks dotfiles/bin /usr/local/bin $3
+make_symlinks dotfiles/bin /usr/local/bin $1
+make_symlinks dotfiles/xbar-plugins "$HOME/Library/Application Support/xbar/plugins" $1
 
