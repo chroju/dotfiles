86dd94b7584bdedcee06e17e0e44b02780068745
chroju <chroju@users.noreply.github.com>
2022-11-14 19:46:46 +0900

    add: raycast scripts

---
+++ dotfiles/HOME/raycast/pomodoro.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title pomodoro
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🍅
+
+# Documentation:
+# @raycast.author chroju
+# @raycast.authorURL https://github.com/chroju
+
+/Users/jutaro.numata/Library/Application\ Support/xbar/plugins/pomodoro.1s.sh work
