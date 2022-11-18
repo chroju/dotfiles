4c900bcd911f0b00c99243028023974308864ff7
chroju <chroju@users.noreply.github.com>
2022-11-18 20:24:01 +0900

    mod: stop supporting intel mac

--- setup_homebrew.sh
+++ setup_homebrew.sh
@@ -2,34 +2,18 @@
 
 xcode-select --install || echo "xcode-select --install is alredy done."
 
-arch=$(uname -m)
+if (type "/usr/local/bin/brew" > /dev/null 2>&1); then
+  echo "brew (for Rosetta) already installed."
+else
+  arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
 
-case $arch in
-"arm64")
-  # install brew for Rosetta
-  if (type "/usr/local/bin/brew" > /dev/null 2>&1); then
-    echo "brew (for Rosetta) already installed."
-  else
-    arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    /usr/local/bin/brew bundle --file=./Brewfiles/arm/iBrewfile
-  fi
-
-  # install brew for arm
-  if (type "/opt/homebrew/bin/brew" > /dev/null 2>&1); then
-    echo "brew (for arm) already installed."
-  else
-    mkdir -p /opt/homebrew
-    chown -R $(whoami):staff /opt/homebrew
-    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /opt/homebrew
-    /opt/homebrew/bin/brew bundle --file=./Brewfiles/arm/Brewfile
-  fi
-  ;;
-*)
-  if (type "/usr/local/bin/brew" > /dev/null 2>&1); then
-    echo "brew already installed."
-  else
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    /usr/local/bin/brew bundle --file=./Brewfiles/intel/Brewfile
-  fi
-  ;;
-esac
+# install brew for arm
+if (type "/opt/homebrew/bin/brew" > /dev/null 2>&1); then
+  echo "brew (for arm) already installed."
+else
+  mkdir -p /opt/homebrew
+  chown -R $(whoami):staff /opt/homebrew
+  curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /opt/homebrew
+  /opt/homebrew/bin/brew bundle --file=./Brewfile
+fi
