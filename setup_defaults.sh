2a7488346c658f54c8ca49ee822e57e0ff3efafe
chroju <chroju@users.noreply.github.com>
2022-11-18 20:10:53 +0900

    add: defaults

--- setup_defaults.sh
+++ setup_defaults.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Theme
+defaults write 'Apple Global Domain' AppleInterfaceStyle Dark
+
+# Trackpad
+defaults write com.apple.AppleMultitouchTrackpad Clicking 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking 1
+
+# Dock
+defaults write com.apple.dock autohide 1
+defaults write com.apple.dock largesize 128
+defaults write com.apple.dock minimize-to-application 1
+
 # https://github.com/VSCodeVim/Vim#mac
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
