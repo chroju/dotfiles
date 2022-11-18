6e4095f1cf327393262b1f892a6cd11f7b0c0987
chroju <chroju@users.noreply.github.com>
2022-11-18 20:39:56 +0900

    mod: setup scripts

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-if !(type "brew" > /dev/null 2>&1); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-#brew bundle
-#ansible-playbook playbook.yml -K
-
 
 function make_symlinks() {
     for f in $(ls -A $1); do
