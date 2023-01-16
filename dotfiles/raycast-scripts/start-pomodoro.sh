e5ea55353d2cebd6ecaef8bcfec866bc45557551
chroju <chroju@users.noreply.github.com>
2023-01-16 09:45:57 +0900

    pomodoroにタスクを記録する

---
+++ dotfiles/raycast-scripts/start-pomodoro.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Start pomodoro
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🍅
+# @raycast.argument1 { "type": "text", "placeholder": "What to do ?", "percentEncoded": false }
+
+# Documentation:
+# @raycast.author chroju
+# @raycast.authorURL https://github.com/chroju
+
+'/Users/chroju/Library/Application Support/xbar/plugins/pomodoro.1s.sh' work $1
+
